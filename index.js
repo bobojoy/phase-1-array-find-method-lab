75a7b0b111eca52c478d4ec2a9f126c7e7986dcb
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-07-07 12:24:49 +0300

    complete

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // code your solution here
-const record =[
-    {year:'2015', result:'W'},
-    {year:'2014', result:'N/A'},
-    {year:'2013', result:'L'}
+const record = [
     
-]
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+   
+   
+  ]
 
-function superbowlWin(record) {
-    const win =record.find(obj => obj.result === 'W')
-    return win ? win.year : undefined;
-}
+
+  function superbowlWin(record) {
+    const win = record.find(item => item.result === "W");
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
